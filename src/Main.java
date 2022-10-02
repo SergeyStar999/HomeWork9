71667aee1f842f056abaf4140e40374e69d69e49
Сергей <sergeystarod@mail.ru>
2022-10-02 22:08:12 +0300

    Мой первый коммит

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        var aythor1 = new Author("Артём", "Долгов");
+        var aythor2 = new Author("Роберт", "Киосаки");
+        var book1 = new Book("Игра. ", aythor1, 1996);
+        var book2 = new Book("Конец. ", aythor2, 1969);
+        System.out.println(book1);
+        System.out.println(book2);
     }
 }
